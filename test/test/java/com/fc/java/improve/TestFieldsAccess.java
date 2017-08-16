9d739eb1786c0f5aa83cc16447261d62dc7430da
fangcong <1070271203@qq.com>
2017-08-16 11:20:38 +0800

    react-demo

--- test/test/java/com/fc/java/improve/TestFieldsAccess.java
+++ test/test/java/com/fc/java/improve/TestFieldsAccess.java
@@ -18,21 +18,21 @@ public class TestFieldsAccess {
             System.out.println("getFields return :");
             Field[] fields = clazz.getFields();
             for (Field field : fields) {
-                System.out.println(field);
+                System.out.println(field.getName());
             }
 
             //能获取bean所有定义属性
             System.out.println("getDeclaredFields return :");
             Field[] fields1 = clazz.getDeclaredFields();
             for (Field field : fields1) {
-                System.out.println(field);
+                System.out.println(field.getName());
             }
 
             //获取指定名称属性，访问限制和获取所有一致
             Field field1 = clazz.getField("field1");
-            System.out.println("getField : " + field1);
+            System.out.println("getField : " + field1.getName());
             Field field2 = clazz.getDeclaredField("field5");
-            System.out.println("getDeclaredField : " + field2);
+            System.out.println("getDeclaredField : " + field2.getName());
 
             //测试获取bean定义方法时，同构方法是否传参返回值
             Method method = clazz.getDeclaredMethod("method1");
